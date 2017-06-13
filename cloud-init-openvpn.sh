2b2b983a3623d0cfe1011a75087a550bd387d88b
billyprice1 <lkk29898@email.vccs.edu>
2017-06-13 17:09:02 +0100

    Create cloud-init-openvpn.sh

--- cloud-init-openvpn.sh
+++ cloud-init-openvpn.sh
@@ -4,12 +4,12 @@ apt-get -y upgrade ;
 apt-get -y install wget ;
 wget -qO- https://get.docker.com/ | sh
 export OVPN_DATA=ovpn-data ;
-docker pull billy/openvpn ;
+docker pull kylemanna/openvpn ;
 docker volume create --name $OVPN_DATA ;
-docker run -v $OVPN_DATA:/etc/openvpn --rm billy/openvpn ovpn_genconfig -u udp://$(curl ifconfig.co) ;
+docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://$(curl ifconfig.co) ;
 
 # manual from here on, you need a password
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it billy/openvpn ovpn_initpki
-docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --name openvpn --cap-add=NET_ADMIN billy/openvpn
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it billy/openvpn easyrsa build-client-full a_client nopass
-docker run -v $OVPN_DATA:/etc/openvpn --rm billy/openvpn ovpn_getclient a_client > a_client.ovpn
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
+docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --name openvpn --cap-add=NET_ADMIN kylemanna/openvpn
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full a_client nopass
+docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient a_client > vpnconfig.ovpn
